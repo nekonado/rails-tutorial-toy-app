23c0741f7460608a932d2ba93fc279e259ebcaf5
nekonado <nekonadocat@gmail.com>
2023-11-23 15:47:59 +0000

    generate micropost by scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_103135) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_152733) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
